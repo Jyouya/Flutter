4478f56433c748648fedf8dd57d9d39adf4ab59b
Grayson Gantek <graysonleegantek@gmail.com>
2019-08-28 12:22:05 -0400

    nav svg's

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,14 +4,5 @@ module.exports = app => {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-
-    // app.get("/main.css", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/css/main.css"));
-    // });
-
-    // app.get("/reset.css", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/css/reset.css"));
-    // });
-
 }
 
