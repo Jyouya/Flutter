630be787d105b74e5faeaf463d9616f1ee1485bd
Grayson Gantek <graysonleegantek@gmail.com>
2019-08-29 12:13:17 -0400

    quick server fix

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const fs = require ('fs');
 const app = express();
 const path = require("path");
 
-const models = ("./models");
-
 const PORT = process.env.PORT || 8443;
+
+const models = require("./models");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -16,21 +16,15 @@ app.use(express.json());
 
 require('./routes/html-routes')(app);
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT + '! Go to https://localhost:' + PORT)
+models.sequelize.sync().then(() => {
+
+  https.createServer({
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+  }, app).listen(PORT, function () {
+      console.log('Example app listening on port ' + PORT + '! Go to https://localhost:' + PORT)
+  });
+
 });
-
-// if (process.env.NODE_ENV !== 'test') {
-//     models.sequelize.sync().then(() => {
-//         app.listen(PORT, function () {
-//             console.log('App listening on PORT ' + PORT);
-//         });
-//     });
-
-// }
 
 module.exports = app; // for testing
