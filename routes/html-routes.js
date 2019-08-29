01a72005a236af49a5164c2fc7157643a426d9b8
Grayson Gantek <graysonleegantek@gmail.com>
2019-08-29 11:35:58 -0400

    working on html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
-const express = require('express')
-const fs = require('fs')
-const https = require('https')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Working Title')
-})
-
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(3000, function () {
-  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
-})
-=======
-const path = require("path");
-
-module.exports = app => {
-    app.get("/", function (req, res) {
+module.exports = function (app) {
+    app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-    app.get("/account", function (req, res) {
+    app.get('/account', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/account.html"));
     });
 }
-
->>>>>>> fae7482d114bf5ee9bbd2e9e95d6fa33c4611a43
--- server.js
+++ server.js
@@ -3,20 +3,17 @@ const express = require('express');
 const fs = require ('fs');
 const app = express();
 const PORT = process.env.PORT || 8443;
-app.get('/', function (req, res) {
-  res.send('Working-Title')
-})
-
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(8443, function () {
-  console.log('Example app listening on port 8443! Go to https://localhost:8443/')
-})
-
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const html = require('./routes/html-routes')(app);
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT + '! Go to https://localhost:' + PORT)
+})
