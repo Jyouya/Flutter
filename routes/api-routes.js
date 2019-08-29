d15e52ce8aad89bec927d99f591998f41a420df8
Will <willwest8xp@gmail.com>
2019-08-29 07:44:06 -0400

    separated verify function from routing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,12 @@
 const db = require('../models');
 const auth = require('./auth');
+const verify = require('../verify');
 
 
 module.exports = (app) => {
-    const verify = auth(app, db); // POST routes for /api/login, /api/users
+    auth(app, db); // Create POST routes for /api/login and /api/users
 
+    // A test route for automated testing.  Don't change.
     app.post('/api/authtest', async function (req, res) {
         try {
             const { userId, type } = await verify(req.body.jwt, req);
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const { promisify } = require('util')
+const { promisify } = require('util');
 const crypto = require('crypto');
 const scryptP = promisify(crypto.scrypt);
 const randomBytesP = promisify(crypto.randomBytes);
@@ -95,50 +95,5 @@ module.exports = function (app, db) {
             return false;
         }
         return user;
-    }
-    return async function verify(token, req) {
-        // Throws error if token wasn't signed by us.
-        const decoded = await jwtVerifyP(token, process.env.JWT_SECRET);
-
-        // Throw an error if the token is not issued to the client making the request.
-        if (decoded.ip != requestIp.getClientIp(req)) throw "Authentication Failed";
-
-        // Look up the token in our database.  Throws an error if we don't have a record of it.
-        dbToken = await db.Token.findOne({
-            where: {
-                id: decoded.tokenId
-            }
-        });
-
-
-        // Check if the token has expired.  If so, delete it from the db and throw an error
-        if (dbToken.updatedAt * 1000 + 1200000 < new Date().getTime()) {
-            db.Token.delete({
-                where: {
-                    id: dbToken.id
-                }
-            });
-            throw "Authentication Failed";
-        }
-
-        // Authentication is successful at this point
-
-        // Refresh the timeout on the token
-        db.Token.update({ id: dbToken.id }, {
-            where: {
-                id: dbToken.id
-            }
-        }).then(async result => {
-            const token = await db.Token.findOne({
-                where: {
-                    id: dbToken.id
-                }
-            });
-        });
-
-        return {
-            userId: decoded.userId,
-            type: decoded.type,
-        }
     }
 };
