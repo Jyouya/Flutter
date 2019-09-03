128199ebfef35875e92da1902922dda8ea063580
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-03 12:38:15 -0400

    detecting for positive or negative scroll

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,1 +1,11 @@
 console.log("Scroll script loaded");
+
+$(window).on('mousewheel', function(event){
+    //code that will only fire on manual scroll input
+    const delta = event.originalEvent.deltaY;
+    if (delta > 0) {
+        console.log("scrolling down");
+    } else if (delta <= 0) {
+        console.log("scrolling up");
+    }
+});
