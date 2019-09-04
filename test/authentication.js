610b9ad48da14c35ecc8e235ff6e35088676f7e2
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-04 12:28:18 -0400

    moves config to environment variables

--- test/authentication.js
+++ test/authentication.js
@@ -1,5 +1,6 @@
-process.env.NODE_ENV = 'test';
+// process.env.NODE_ENV = 'test';
 
+require('dotenv').config();
 const db = require('../models');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
